543dee02ad6ef4939659220b34852a8e41606dd0
fabio_magrotti <fabio.magrotti@gmail.com>
2022-02-23 10:03:02 +0100

    test commit

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ SECRETS = "config_files/secrets.json"
 Riot = lib.RiotLoLAPI()
 Riot.load_config(CONFIGS, SECRETS)
 
-data = Riot.get_champion_info(champ_name="", champ_id=50)
-#print(json.dumps(data, indent=4, ensure_ascii=False))
+# EXAMPLE
+data = Riot.get_champion_info(champ_name="Thresh")
+print(json.dumps(data, indent=4, ensure_ascii=False))
