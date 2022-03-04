a92de0986b915f319319dc3f57c7c5ad5c7a3901
fabio_magrotti <fabio.magrotti@gmail.com>
2022-03-04 16:48:16 +0100

    updated examples

--- examples/champion.py
+++ examples/champion.py
@@ -1,13 +1,13 @@
 """Contains an example of how the Champion class should be instanced and a few use of it"""
 import json
-from catlyn.config import settings
-from catlyn.shared import utils
+from catlyn import Catlyn, Settings
 
 CONFIGS = "example_config.json"
 SECRETS = "../catlyn/config/secrets.json"
 
-SETTINGS = settings.Settings(CONFIGS, SECRETS)
+SETTINGS = Settings(CONFIGS, SECRETS)
+catlyn = Catlyn(settings=SETTINGS)
 
 # EXAMPLE 1
-data = utils.get_champion_by_id(settings=SETTINGS, champ_id="888")
+data = catlyn.get_champion_by_id(champ_id="888")
 print(json.dumps(data, indent=4, ensure_ascii=False))
